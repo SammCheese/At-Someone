feda2a3a8c58a13eedb0e2a899f5b730c0029c1e
Vendicated <vendicated@riseup.net>
2021-05-08 05:58:53 +0200

    :D

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
 /* eslint-disable indent */
-const { Plugin } = require('powercord/entities');
-const { inject, uninject } = require('powercord/injector');
-const { findInReactTree } = require('powercord/util');
-const { getModule } = require('powercord/webpack');
+const { Plugin } = require("powercord/entities");
+const { inject, uninject } = require("powercord/injector");
+const { getModule, React } = require("powercord/webpack");
 
-function getRandomUserID () {
-    const Members = getModule([ 'getMemberIds' ], false)
-        .getMemberIds(getModule([ 'getLastSelectedGuildId' ], false)
-        .getLastSelectedGuildId());
-    return Members[Math.floor(Math.random() * Members.length)];
-}
+const {
+  AUTOCOMPLETE_OPTIONS: { MENTIONS }
+} = getModule(["AUTOCOMPLETE_OPTIONS"], false);
+
+const Someone = require("./Someone");
 
 module.exports = class atSomeone extends Plugin {
-    async startPlugin () {
-        this._injectAutoComplete();
-        powercord.api.commands.registerCommand({
-            command:    '@/Someone',
-            aliases:    [ '@s' ],
-            description:    'Ats Someone',
-            usage:  '',
-            async executor () {
-                return {
-                    send: true,
-                    result: `<@${getRandomUserID()}>`
-                };
-            }
-        });
-    }
+  async startPlugin() {
+    this._injectAutoComplete();
+    powercord.api.commands.registerCommand({
+      command: "@/Someone",
+      aliases: ["@s"],
+      description: "Ats Someone",
+      usage: "",
+      async executor() {
+        return {
+          send: true,
+          result: `<@${getRandomUserID()}>`
+        };
+      }
+    });
+  }
 
-    async _injectAutoComplete () {
-        const { AUTOCOMPLETE_OPTIONS: { MENTIONS } } = getModule([ 'AUTOCOMPLETE_OPTIONS' ], false);
-        inject('as-mention-container', MENTIONS, 'renderResults', (args, res) => {
-        if (args[7].globals.length === 2 || args[7].globals.length === 0) {
-            args[7].globals.push({
-                description: 'Mentions Someone!',
-                text: '@someone'
-            });
-        }
-        try {
-            const sex = findInReactTree(res, ({ key }) => key === '@someone');
-            sex.props.onClick = () => {
-                const currentChannel = require('powercord/webpack').channels.getChannelId();
-                require('powercord/webpack').getModule([ 'sendMessage' ], false)
-                    .sendMessage(currentChannel, { content: '<@' + getRandomUserID() + '>' });
-            };
-        } catch {}
-        return res;
-        });
-    }
+  _injectAutoComplete() {
+    inject("as-mention-container", MENTIONS, "renderResults", (args, res) => {
+      if (!"someone".includes(args[1].toLowerCase())) return res;
 
-    pluginWillUnload () {
-        powercord.api.commands.unregisterCommand('@/Someone');
-        uninject('as-mention-container');
-    }
+      res.props.children[3]?.unshift(React.createElement(Someone));
+      return res;
+    });
+  }
+
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand("@/Someone");
+    uninject("as-mention-container");
+  }
 };
