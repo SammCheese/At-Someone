251545741c3918036bed5cdb759ca5afab2d5bfc
SammCheese <52699291+SammCheese@users.noreply.github.com>
2021-05-08 03:22:28 +0200

    You can now @ someone from the @ menu!

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* eslint-disable indent */
 const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
 const { getModule } = require('powercord/webpack');
 
 function getRandomUserID () {
@@ -10,7 +12,8 @@ function getRandomUserID () {
 }
 
 module.exports = class atSomeone extends Plugin {
-    startPlugin () {
+    async startPlugin () {
+        this._injectAutoComplete();
         powercord.api.commands.registerCommand({
             command:    '@/Someone',
             aliases:    [ '@s' ],
@@ -25,7 +28,29 @@ module.exports = class atSomeone extends Plugin {
         });
     }
 
+    async _injectAutoComplete () {
+        const { AUTOCOMPLETE_OPTIONS: { MENTIONS } } = getModule([ 'AUTOCOMPLETE_OPTIONS' ], false);
+        inject('as-mention-container', MENTIONS, 'renderResults', (args, res) => {
+        if (args[7].globals.length === 2 || args[7].globals.length === 0) {
+            args[7].globals.push({
+                description: 'Mentions Someone! [Not Yet]',
+                text: '@someone'
+            });
+        }
+        try {
+            const sex = findInReactTree(res, ({ key }) => key === '@someone');
+            sex.props.onClick = () => {
+                const currentChannel = require('powercord/webpack').channels.getChannelId();
+                require('powercord/webpack').getModule([ 'sendMessage' ], false)
+                    .sendMessage(currentChannel, { content: '<@' + getRandomUserID() + '>' });
+            };
+        } catch {}
+        return res;
+        });
+    }
+
     pluginWillUnload () {
         powercord.api.commands.unregisterCommand('@/Someone');
+        uninject('as-mention-container');
     }
 };
